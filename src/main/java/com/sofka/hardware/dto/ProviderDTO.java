827c2b36d6018d5ff28dff6fb4fa0e5281669dcb
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-09 13:59:59 -0500

    DTO's v1.0

---
+++ src/main/java/com/sofka/hardware/dto/ProviderDTO.java
@@ -0,0 +1,16 @@
+package com.sofka.hardware.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class ProviderDTO {
+    private String idProvider;
+    @NotBlank(message="nameProvider cant be blank")
+    private String nameProvider;
+    @NotBlank(message="dniProvider cant be blank")
+    private String dniProvider;
+    @NotBlank(message="phoneProvider cant be blank")
+    private String phoneProvider;
+}
