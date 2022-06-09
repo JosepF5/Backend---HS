1b82e32f1ea2ed16ac263f39d37fc6d5284860ef
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-09 14:09:43 -0500

    Mapper's v1.0

---
+++ src/main/java/com/sofka/hardware/mapper/ProviderMapper.java
@@ -0,0 +1,25 @@
+package com.sofka.hardware.mapper;
+
+import com.sofka.hardware.dto.ProviderDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.Provider;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Component
+@EnableWebFlux
+public class ProviderMapper {
+    private final ModelMapper mapper;
+
+    public ProviderMapper(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public ProviderDTO toProviderDto(Provider provider){
+        return mapper.map(provider, ProviderDTO.class);
+    }
+
+    public Provider toProviderEntity(ProviderDTO providerDto){
+        return mapper.map(providerDto, Provider.class);
+    }
+}
