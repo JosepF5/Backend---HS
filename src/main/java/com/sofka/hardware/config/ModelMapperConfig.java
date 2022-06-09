5d6e46e8e3af20b654377fde332e1fe63c4d2046
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-09 14:02:36 -0500

    Config's v1.0

---
+++ src/main/java/com/sofka/hardware/config/ModelMapperConfig.java
@@ -0,0 +1,16 @@
+package com.sofka.hardware.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+
+}
