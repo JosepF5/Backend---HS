38e4c0f0efd4020c0823f3a6ed6525c104950f90
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-11 03:21:59 -0500

    Repository's v1.2

---
+++ src/main/java/com/sofka/hardware/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sofka.hardware.repository;
+
+import com.sofka.hardware.collection.Product;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product,String> {
+
+}
