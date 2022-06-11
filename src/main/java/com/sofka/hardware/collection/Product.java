5d8aef96e518ff520b32606a1655867609454af7
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-11 03:21:27 -0500

    Collection's v1.2

--- src/main/java/com/sofka/hardware/collection/Product.java
+++ src/main/java/com/sofka/hardware/collection/Product.java
@@ -1,96 +1,32 @@
 package com.sofka.hardware.collection;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.Objects;
 
+@Data
+@Document(collection="product")
 public class Product {
 
-    private String name;
+    @Id
+    private String idProduct;
+    @NotBlank(message="nameProduct cant be blank")
+    private String nameProduct;
+    @NotBlank(message="amountProduct cant be blank")
+    private Integer amountProduct;
+    @NotBlank(message="minAmountProduct cant be blank")
+    private Integer minAmountProduct;
+    @NotBlank(message="maxAmountProduct cant be blank")
+    private Integer maxAmountProduct;
+    @NotBlank(message="providersProduct cant be blank")
+    private String providersProduct;
+    @NotBlank(message="descriptionProduct cant be blank")
+    private String descriptionProduct;
+    @NotBlank(message="priceProduct cant be blank")
+    private Long priceProduct;
 
-    private Integer amount;
-
-    private Provider providers;
-
-    private String description;
-
-    private Long price;
-
-    public Product(String name,
-                   Integer amount,
-                   Provider providers,
-                   String description,
-                   Long price) {
-        this.name = name;
-        this.amount = amount;
-        this.providers = providers;
-        this.description = description;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
-    public Provider getProviders() {
-        return providers;
-    }
-
-    public void setProviders(Provider providers) {
-        this.providers = providers;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return name.equals(product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "name='" + name + '\'' +
-                ", amount=" + amount +
-                ", providers=" + providers +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                '}';
-    }
 }
