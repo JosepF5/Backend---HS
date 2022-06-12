dd6c358cb65e6c99ddb9f8543abb90b77b0526c3
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-12 00:47:54 -0500

    Collection's v1.4 (Receipt)

--- src/main/java/com/sofka/hardware/collection/Receipt.java
+++ src/main/java/com/sofka/hardware/collection/Receipt.java
@@ -1,4 +1,22 @@
 package com.sofka.hardware.collection;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Document(collection = "receipt")
 public class Receipt {
+    @Id
+    private String idReceipt;
+    @NotBlank(message="dateReceipt can't be be blank")
+    private String dateReceipt;
+    @NotBlank(message="nameProviderReceipt can't be be blank")
+    private String nameProviderReceipt;
+    @NotBlank(message="productReceipt can't be be blank")
+    private String productReceipt;
+    @NotBlank(message="amountReceipt can't be be blank")
+    private Integer amountReceipt;
 }
