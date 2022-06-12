e69783b32a90a8f1bd3893660da844a65aa25f58
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-11 23:04:30 -0500

    Collection's v1.3

--- src/main/java/com/sofka/hardware/collection/Bill.java
+++ src/main/java/com/sofka/hardware/collection/Bill.java
@@ -11,20 +11,20 @@ import java.util.List;
 @Document(collection = "bill")
 public class Bill {
     @Id
-    private String id;
+    private String idBill;
 
-    @NotBlank(message="Date can't be be blank")
-    private String date;
+    @NotBlank(message="dateBill can't be be blank")
+    private String dateBill;
 
-    @NotBlank(message="Client can't be blank")
-    private String client;
+    @NotBlank(message="clientBill can't be blank")
+    private String clientBill;
 
-    @NotBlank(message="Employee can't be be blank")
-    private String employee;
+    @NotBlank(message="employeeBill can't be blank")
+    private String employeeBill;
 
-    @NotBlank(message="Products can't be be blank")
-    private List<Product> products;
+    @NotBlank(message="productsBill can't be blank")
+    private List<Product> productsBill;
 
-    @NotBlank(message="TotalPaid can't be be blank")
-    private String totalPaid;
+    @NotBlank(message="paymentBill can't be blank")
+    private Integer paymentBill;
 }
