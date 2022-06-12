07f28fcb79aa3b6080a8e1863109276e835fa431
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-11 23:06:26 -0500

    Swagger's v1.0

--- src/main/java/com/sofka/hardware/HardwareApplication.java
+++ src/main/java/com/sofka/hardware/HardwareApplication.java
@@ -1,9 +1,12 @@
 package com.sofka.hardware;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "HardwareApplication", version = "1.0", description = "Documentation APIs v1.0"))
 public class HardwareApplication {
 
 	public static void main(String[] args) {
