f929116ecc57bcbf2e0d45bb6670f65caa490e09
Josep Palomino <jfpalomino.22@gmail.com>
2022-06-11 23:05:20 -0500

    Route's v1.3

---
+++ src/main/java/com/sofka/hardware/route/CreateBillRoute.java
@@ -0,0 +1,43 @@
+package com.sofka.hardware.route;
+
+import com.sofka.hardware.collection.Bill;
+import com.sofka.hardware.dto.BillDTO;
+import com.sofka.hardware.usecase.CreateBillUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class CreateBillRoute {
+    @Bean
+    @RouterOperation(path = "/create/bill", produces = {
+            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.POST, beanClass = CreateBillUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "createBill", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Bill.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid Bill details supplied")}
+                    , requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = Bill.class)))
+            ))
+    public RouterFunction<ServerResponse> createBill(CreateBillUseCase createBillUseCase){
+        return route(POST("/create/bill").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(BillDTO.class)
+                        .flatMap(createBillUseCase::createBill)
+                        .flatMap(billDTO -> ServerResponse.status(HttpStatus.CREATED)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(billDTO))
+                        .onErrorResume(e -> ServerResponse.status(HttpStatus.BAD_REQUEST).build()));
+    }
+}
